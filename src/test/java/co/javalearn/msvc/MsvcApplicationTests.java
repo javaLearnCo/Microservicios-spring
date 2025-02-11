0ee5c5ed0985ec8eed9c46e5b775222c1bc3e60a
Diego Hernandez <usercall.hm@gmail.com>
2025-02-11 16:17:18 +0100

    add base project

---
+++ src/test/java/co/javalearn/msvc/MsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.javalearn.msvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
